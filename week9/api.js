3ee328061b6711a9585c1f5e178ebb0522332160
Kamil Agdere <29575@ma-web.nl>
2020-04-14 18:01:48 +0200

    alles af

--- week9/api.js
+++ week9/api.js
@@ -5,18 +5,28 @@
 //          bekend zijn.
 
 let results = [];
-let item, coordinates, address, = {};
+let item, coordinates, address, id, email, street, timezone, gender, login, dob, registered, phone, cell, picture, nat = {};
 
 function users(items) {
     for (let count = 0; count < items.length; count++) {
 
-        item = new Name(items[count].name)
-        coordinates = new Coordinates(items[count].location.coordinates)
-        address = new Address(items[count].location)
+        item = new Name(items[count].name);
+        coordinates = new Coordinates(items[count].location.coordinates);
+        address = new Address(items[count].location);
+        email = new Email(items[count].email);
+        timezone = new Timezone(items[count].location.timezone);
+        gender = new Gender(items[count].gender);
+        login = new Login(items[count].login);
+        dob = new Dob(items[count].dob);
+        registered = new Registered(items[count].registered);
+        phone = new Phone(items[count].phone);
+        cell = new Cell(items[count].cell);
+        picture = new Picture(items[count].picture);
+        nat = new Nat(items[count].nat);
     }
-    console.table(item)
-    console.table(coordinates)
-    console.table(address.showStreet())
+    console.table(item);
+    console.table(coordinates);
+    console.table(address.showStreet());
 
     // TODO:    Zorg ervoor dat de volgende statements resultaat gaan opleveren
     //          Hiervoor moet je de functie showAddress() afmaken en de constructors voor de
@@ -27,17 +37,26 @@ function users(items) {
     console.table(email);
     console.table(street);
     console.table(address);
+    console.table(timezone);
+    console.table(gender);
+    console.table(login);
+    console.table(dob);
+    console.table(registered);
+    console.table(phone);
+    console.table(cell);
+    console.table(picture);
+    console.table(nat);
 }
 
 function createGetRequest(url, callBack) {
 
-    var request = new XMLHttpRequest()
+    let request = new XMLHttpRequest();
 
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
 
-            let response = JSON.parse(this.response)
-            callBack(response)
+            let response = JSON.parse(this.response);
+            callBack(response);
 
         }
     }
@@ -46,17 +65,17 @@ function createGetRequest(url, callBack) {
         'GET',
         url,
         true
-    )
+    );
 
-    request.send()
+    request.send();
 }
 
 function randomUser(json) {
-    results = json.results.slice()
-    users(results)
+    results = json.results.slice();
+    users(results);
 }
 
-var url = 'https://randomuser.me/api/?results=1'
+let url = 'https://randomuser.me/api/?results=1';
 createGetRequest(url, randomUser);
 // TODO:    check de xhr tab en waarschuwingen tab
 //          De xhr tab heeft subtabs, check deze allemaal
